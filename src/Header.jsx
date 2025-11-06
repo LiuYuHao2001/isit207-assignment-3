22c432310e22dbf465f3db76cf264827979abca5
LiuYuHao2001 <90831062+LiuYuHao2001@users.noreply.github.com>
2025-11-06 15:44:54 +0800

    add header

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <header>
+            <h1>Pet Heaven</h1>
+            <nav>
+                <a href='#'>Adopt</a>
+                <a href='#'>Release</a>
+                <a href='#'>Contact</a>
+                <a href='#'>About Us</a>
+                <a href='#'>Log In</a>
+            </nav>
+        </header >
+    );
+}
+
+export default Header
