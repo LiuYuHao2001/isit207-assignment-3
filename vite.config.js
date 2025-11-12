b15e9d06044354486d7e082454b520edc15c72ef
LiuYuHao2001 <90831062+LiuYuHao2001@users.noreply.github.com>
2025-11-13 00:32:53 +0800

    re-init with vite (attempt #2)

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://liuyuhao2001.github.io/isit207-assignment-3/"
+  base: '/isit207-assignment-3/',
 })
