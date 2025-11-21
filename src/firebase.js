b5660d1adf26c519247bbcc1d6c83939dc98015c
LiuYuHao2001 <90831062+LiuYuHao2001@users.noreply.github.com>
2025-11-22 01:20:48 +0800

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAHASAL-lU27jZ0O50XafihIczwfxmOFeU",
+    authDomain: "isit207-assignment-3-7672111.firebaseapp.com",
+    projectId: "isit207-assignment-3-7672111",
+    storageBucket: "isit207-assignment-3-7672111.firebasestorage.app",
+    messagingSenderId: "402504725540",
+    appId: "1:402504725540:web:06ce748185edcf6a2bd4b1",
+    measurementId: "G-H1F7466QY9"
+};
+
+const app = initializeApp(firebaseConfig);
