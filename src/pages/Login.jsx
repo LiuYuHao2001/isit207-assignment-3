ecfe84b6e4409a874f644486e3b481acf02344b5
LiuYuHao2001 <90831062+LiuYuHao2001@users.noreply.github.com>
2025-11-22 19:33:29 +0800

    fix log in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import styles from "./Login.module.css";
 
 const Login = ({ onLogin }) => {
